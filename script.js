72dae5138b8194fb7251e9dd71e31632054559fb
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-04-23 11:49:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+const cssRoot = document.documentElement;
+const table = document.querySelector('table');
+const playerImg = document.querySelector('img');
+let cordX = 0, cordY = 0;
+
+let sizeString = getComputedStyle(cssRoot).getPropertyValue("--size");
+let sizeNum = parseFloat(sizeString);
+let playerSizeString = getComputedStyle(cssRoot).getPropertyValue("--playerSize");
+let playerSizeNum = parseFloat(playerSizeString);
+upZoom(); downZoom();
+
+document.addEventListener('keydown', function(event) {
+    const targetRow = table.rows[cordY];
+    const targetCell = targetRow.cells[cordX];
+    //Não permitir atravessar paredes
+    if (event.key === 'w') {
+        moveUp();
+    } else if (event.key === 's') {
+        moveDown();
+    } else if (event.key === 'a') {
+        moveLeft();
+    } else if (event.key === 'd') {
+        moveRight();
+    }
+    updatePosition();
+});
+
+function moveUp() {
+    if (cordY > 0) {
+        cordY -= 1;
+    }
+}
+
+function moveDown() {
+    if (cordY < table.rows.length - 1) {
+        cordY += 1;
+    }
+}
+
+function moveLeft() {
+    if (cordX > 0) {
+        cordX -= 1;
+    }
+}
+
+function moveRight() {
+    if (cordX < table.rows[cordY].cells.length - 1) {
+        cordX += 1;
+    }
+}
+
+function updatePosition() {
+    targetRow = table.rows[cordY];
+    targetCell = targetRow.cells[cordX];
+    targetCell.appendChild(playerImg);
+}
+
+function upZoom() {
+    sizeNum += 10;
+    cssRoot.style.setProperty('--size', sizeNum + "px");
+    playerSizeNum = sizeNum - 10;
+    cssRoot.style.setProperty('--playerSize', playerSizeNum + "px");
+}
+
+function downZoom() {
+    sizeNum -= 10;
+    cssRoot.style.setProperty('--size', sizeNum + "px");
+    playerSizeNum = sizeNum - 10;
+    cssRoot.style.setProperty('--playerSize', playerSizeNum + "px");
+}
