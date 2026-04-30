407259f75c8e464a354653851c727705aab025be
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-04-30 13:26:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 const cssRoot = document.documentElement;
 const table = document.querySelector('table');
 const playerImg = document.querySelector('img');
-let cordX = 0, cordY = 0;
 
+let cordX = 0, cordY = 0;
 let sizeString = getComputedStyle(cssRoot).getPropertyValue("--size");
 let sizeNum = parseFloat(sizeString);
 let playerSizeString = getComputedStyle(cssRoot).getPropertyValue("--playerSize");
 let playerSizeNum = parseFloat(playerSizeString);
 upZoom(); downZoom();
 
-document.addEventListener('keydown', function(event) {
-    const targetRow = table.rows[cordY];
-    const targetCell = targetRow.cells[cordX];
-    //Não permitir atravessar paredes
+document.addEventListener('keydown', function (event) {
     if (event.key === 'w') {
         moveUp();
     } else if (event.key === 's') {
@@ -25,31 +22,38 @@ document.addEventListener('keydown', function(event) {
     updatePosition();
 });
 
+function isWall(x, y) {
+    const targetCell = table.rows[y].cells[x];
+    return targetCell.classList.contains('wall');
+}
+
 function moveUp() {
-    if (cordY > 0) {
+    if (cordY > 0 && !isWall(cordX, cordY - 1)) {
         cordY -= 1;
     }
 }
 
 function moveDown() {
-    if (cordY < table.rows.length - 1) {
+    if (cordY < table.rows.length - 1 && !isWall(cordX, cordY + 1)) {
         cordY += 1;
     }
 }
 
 function moveLeft() {
-    if (cordX > 0) {
+    if (cordX > 0 && !isWall(cordX - 1, cordY)) {
         cordX -= 1;
     }
 }
 
 function moveRight() {
-    if (cordX < table.rows[cordY].cells.length - 1) {
+    if (cordX < table.rows[cordY].cells.length - 1 && !isWall(cordX + 1, cordY)) {
         cordX += 1;
     }
 }
 
 function updatePosition() {
+    const targetRow = table.rows[cordY];
+    const targetCell = targetRow.cells[cordX];
     targetRow = table.rows[cordY];
     targetCell = targetRow.cells[cordX];
     targetCell.appendChild(playerImg);
